64d0626bb93b7d3c5953627bf35a36dd44a75323
akila99g <indurangakga.19@uom.lk>
2022-09-24 09:20:08 +0530

    Initial code taken out, Navbar implemented

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import logo from "../img/ABC-BANK.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <ul className="navbar-lang">
+        <li>
+          <a href="#">Sinhala</a>
+        </li>
+        <li>
+          <a href="#">English</a>
+        </li>
+        <li>
+          <a href="#">Tamil</a>
+        </li>
+      </ul>
+
+      <ul className="navbar-links">
+        <li>
+          <a href="#">Branches</a>
+        </li>
+        <li>
+          <a href="#">ATMs</a>
+        </li>
+        <li>
+          <a href="#">Overseas</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+
+      <div className="navbar-img">
+        <img src={logo} alt="BANK LOGO"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
