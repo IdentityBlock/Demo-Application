71c9cc7595d0e342e98dd7489cfbc6d345b3581c
akila99g <indurangakga.19@uom.lk>
2022-09-24 09:22:39 +0530

    Buttonbar with options, half implemented, and added to App.js CSS half added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Initial from "./components/Initial";
+import Navbar from "./components/Navbar";
+import Toolbar from "./components/Toolbar";
+import Tabs from "./components/Tabs";
+import Buttonbar from "./components/Buttonbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Initial /> */}
+      <Navbar />
+      <div style={{ height: 20 }}></div>
+      <Toolbar />
+      <Tabs />
+      <span className="apply-for">Apply For:</span>
+      <Buttonbar />
     </div>
   );
 }
---
+++ src/components/Buttonbar.js
@@ -0,0 +1,49 @@
+import cards from "../img/icons8-bank-cards-96.png";
+import current from "../img/icons8-check-book-96.png";
+import deposites from "../img/icons8-investment-96.png";
+import loans from "../img/icons8-stack-of-money-96.png";
+import savings from "../img/icons8-wallet-96.png";
+
+function Buttonbar() {
+  return (
+    <div className="buttonbar">
+      <div className="buttonbar-button">
+        <a href="#">
+          <img src={savings} alt="Savings" />
+          <span>Savings</span>
+          <span>Accounts</span>
+        </a>
+      </div>
+
+      <div className="buttonbar-button">
+        <a href="#">
+          <img src={deposites} alt="Deposites" />
+          <span>Deposites</span>
+        </a>
+      </div>
+
+      <div className="buttonbar-button">
+        <a href="#">
+          <img src={current} alt="Current Accounts" />
+          <span>Current Accounts</span>
+        </a>
+      </div>
+
+      <div className="buttonbar-button">
+        <a href="#">
+          <img src={cards} alt="Cards" />
+          <span>Cards</span>
+        </a>
+      </div>
+
+      <div className="buttonbar-button">
+        <a href="#">
+          <img src={loans} alt="Loans" />
+          <span>Loans</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Buttonbar;
