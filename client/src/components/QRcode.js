52befed11d64704cf2ae29eb58ca3cee230b04c8
KasunIs <wgkasunisuranga@gmail.com>
2022-11-01 16:04:31 +0530

    Data passing from backend

--- client/src/components/QRcode.js
+++ client/src/components/QRcode.js
@@ -6,10 +6,38 @@ function QRcode() {
   const axios = require('axios');
   const [qrcode, setQRcode] = useState(qr);
 
+  console.log("abc");
+
   useEffect(() => {  
-    axios.get('/qr-code').then((data)=>{
-      console.log(data);
+    axios.get('/qr-code').then(async function(data) {
+      //console.log(data);
       setQRcode(data.data);
+
+      for (let i = 1; i < 10; i++) {
+        //let rec = false;
+        console.log(i);
+
+        let data =  await axios.get("http://localhost:3000/data");
+
+        console.log(data);
+        if (! (data === undefined)) {
+          break;
+        }
+
+        //console.log(rec);
+        //if (rec) {
+          //break;
+        //}
+
+        console.log(i);
+        //alert(i);
+
+        let myPromise = new Promise(function(resolve) {
+          setTimeout(function() {resolve();}, 5000 * i);
+        });
+  
+        await myPromise;
+      }
     });
   }, [])
   
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require('express');
 const {request} = require('./library/api');
 
@@ -6,8 +7,21 @@ const app = express();
 app.use(express.json());
 
 app.get("/qr-code", function(req, res){
-    request("ABC-Bank_qr-code-request", ["Name", "Email", "Phone"]).then((data)=>{
+    request("ABC-Bank_qr-code-request", ["Name", "Address"], function(err, data) {
+        //console.log(data);
+        console.log("a");
+        //console.log(res)
         res.send(data);
+    }).then(function(data) {
+        console.log("bc");
+        //axios.get("http://localhost:3000/savings/verified", data);
+        //axios.get("http://localhost:3000", data);
+        //res.send(data);
+        app.get("/data", function(req, res) {
+            console.log("c");
+            console.log(data);
+            res.send(data);
+        });
     })
 });
 
